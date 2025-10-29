f0f88657c8f84dd2df1e1ff929583de148ee8d4d
KhushalDaiya <daiyakhushal9@gmail.com>
2025-10-29 14:12:03 +0530

    Update data.py

--- data.py
+++ data.py
@@ -6,7 +6,7 @@ connection = mysql.connector.connect(host='127.0.0.1',user='root',password="khus
 
 # cursor = connection.cursor()
 
-# query_to_execute = "SELECT * FROM bacha;"
+# query_to_execute = "SELECT * FROM user;"
 
 # cursor.execute(query_to_execute)
 # data  = cursor.fetchall()
@@ -17,13 +17,13 @@ connection = mysql.connector.connect(host='127.0.0.1',user='root',password="khus
 
 cursor = connection.cursor()
 
-name = input("Name dalo: ")
-email = input("Email dalo: ")
-mobile = input("Mobile number dalo: ")
+name = input("Enter Name : ")
+email = input("Enter Email : ")
+mobile = input("Enter Mobile no.: ")
 
 
 if email.endswith("@gmail.com"):
-    query = "INSERT INTO bacha (name, email, mobile) VALUES (%s, %s, %s)"
+    query = "INSERT INTO user (name, email, mobile) VALUES (%s, %s, %s)"
     values = (name, email, mobile)
     cursor.execute(query, values)
     connection.commit()
@@ -32,3 +32,4 @@ if email.endswith("@gmail.com"):
 else:
 
     print("Invalid email! Please enter a Gmail address (must end with @gmail.com).")
+
