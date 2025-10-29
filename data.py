e5e325ed4918a1faee8f31fc0cefad3e2dc2791d
KhushalDaiya <daiyakhushal9@gmail.com>
2025-10-29 16:29:13 +0530

    Add validation for email and mobile inputs

--- data.py
+++ data.py
@@ -9,9 +9,16 @@ mobile = input("Enter Mobile no.: ")
 if name.strip() == "" or email.strip() == "" or mobile.strip() == "":
     print("Name, Email, and Mobile number cannot be empty.")
 
+elif email.endswith("@gmail.com"):
+    print("Email must end with '@gmail.com'.")
+
+elif len(mobile) != 10:
+    print("Mobile number must be exactly 10 digits.")
+    
 else:
     query = "INSERT INTO user (name, email, mobile) VALUES (%s, %s, %s)"
     values = (name, email, mobile)
     cursor.execute(query, values)
     connection.commit()
     print("Data inserted successfully!")
+
